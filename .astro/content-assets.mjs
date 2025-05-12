d217dac814267be4504c0bfaddf338b8072c75d2
mariusbeam <marius@beamlight.it>
2025-05-12 18:03:08 +0200

    Fix image paths by removing leading slash

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -2,5 +2,6 @@
 import __ASTRO_IMAGE_IMPORT_ZxlCEH from "../images/calm.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json";
 import __ASTRO_IMAGE_IMPORT_MFJml from "../images/007.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json";
 import __ASTRO_IMAGE_IMPORT_Z1ICg4T from "/Listings/acasa-in-bucovina/bucovina.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json";
-export default new Map([["../images/calm.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json", __ASTRO_IMAGE_IMPORT_ZxlCEH], ["../images/007.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json", __ASTRO_IMAGE_IMPORT_MFJml], ["/Listings/acasa-in-bucovina/bucovina.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json", __ASTRO_IMAGE_IMPORT_Z1ICg4T]]);
+import __ASTRO_IMAGE_IMPORT_oWheQ from "Listings/acasa-in-bucovina/bucovina.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json";
+export default new Map([["../images/calm.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json", __ASTRO_IMAGE_IMPORT_ZxlCEH], ["../images/007.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json", __ASTRO_IMAGE_IMPORT_MFJml], ["/Listings/acasa-in-bucovina/bucovina.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json", __ASTRO_IMAGE_IMPORT_Z1ICg4T], ["Listings/acasa-in-bucovina/bucovina.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json", __ASTRO_IMAGE_IMPORT_oWheQ]]);
 		
--- .astro/content-modules.mjs
+++ .astro/content-modules.mjs
@@ -1,5 +1,5 @@
 
 export default new Map([
-["src/data/pages/blog.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fpages%2Fblog.mdx&astroContentModuleFlag=true")],
-["src/data/pages/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fpages%2Findex.mdx&astroContentModuleFlag=true")]]);
+["src/data/pages/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fpages%2Findex.mdx&astroContentModuleFlag=true")],
+["src/data/pages/blog.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fdata%2Fpages%2Fblog.mdx&astroContentModuleFlag=true")]]);
 		
