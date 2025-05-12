61fff05b936e0c7b63b9ac29d58a8ce4102fd1f4
mariusbeam <marius@beamlight.it>
2025-05-12 18:01:31 +0200

    Update image paths to use local images

--- .astro/content-assets.mjs
+++ .astro/content-assets.mjs
@@ -1,5 +1,6 @@
 
 import __ASTRO_IMAGE_IMPORT_ZxlCEH from "../images/calm.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json";
 import __ASTRO_IMAGE_IMPORT_MFJml from "../images/007.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json";
-export default new Map([["../images/calm.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json", __ASTRO_IMAGE_IMPORT_ZxlCEH], ["../images/007.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json", __ASTRO_IMAGE_IMPORT_MFJml]]);
+import __ASTRO_IMAGE_IMPORT_Z1ICg4T from "/Listings/acasa-in-bucovina/bucovina.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json";
+export default new Map([["../images/calm.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json", __ASTRO_IMAGE_IMPORT_ZxlCEH], ["../images/007.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json", __ASTRO_IMAGE_IMPORT_MFJml], ["/Listings/acasa-in-bucovina/bucovina.png?astroContentImageFlag=&importer=src%2Fdata%2Fdirectory%2Fdirectory.json", __ASTRO_IMAGE_IMPORT_Z1ICg4T]]);
 		
--- src/data/directory/acasa-in-bucovina.md
+++ src/data/directory/acasa-in-bucovina.md
@@ -5,7 +5,7 @@ title: Acasa in Bucovina
 Short Description: Magazin Romanesc Acasa in Bucovina
 Full description: Magazin Romanesc Acasa in Bucovina
 Submission date: 2025-05-11T22:00:00.000Z
-image: directory-practice/public/Listings/acasa-in-bucovina /bucovina.png
+image: /Listings/acasa-in-bucovina/bucovina.png
 ---
 
 
